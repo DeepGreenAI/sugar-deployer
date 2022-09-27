3c15f9fecfaf97cda1eb95118fa42a6a77f593a3
DeepGreenAI <william@deepgreenai.com>
2022-09-27 10:41:15 -0400

    fix 404 issue with debian package

--- 1-install-sugar-solana.bash
+++ 1-install-sugar-solana.bash
@@ -9,8 +9,9 @@ bash <(curl -sSf https://sugar.metaplex.com/install.sh)
 echo "Updating packages"
 #The following two lines fix an issue with packaging in ubuntu 22.4
 #https://stackoverflow.com/questions/72378647/spl-token-error-while-loading-shared-libraries-libssl-so-1-1-cannot-open-shar
-wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1l-1ubuntu1.6_amd64.deb
-sudo dpkg -i libssl1.1_1.1.1l-1ubuntu1.6_amd64.deb
+#If link is expired you will need to grab a new url from http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/?C=M;O=D 
+wget http://debian.mirror.ac.za/debian/pool/main/o/openssl/libssl1.1_1.1.1o-1_amd64.deb
+sudo dpkg -i libssl1.1_1.1.1o-1_amd64.deb
 
 echo "Sugar succesfully installed. Running --version check"
 /home/ubuntu/bin/sugar --version
